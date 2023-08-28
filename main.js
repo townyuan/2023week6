dbbc049dc9c89e65ae17085fdb04d39b90b837ab
townyuan <townyuan@gmail.com>
2023-08-28 18:17:17 +0800

    更新所有頁面

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 import './assets/scss/all.scss';
 import './node_modules/bootstrap/dist/js/bootstrap.min.js';
-// import './assets/js/all.js';
+import './node_modules/jquery/dist/jquery.min.js';
+import Swiper from 'swiper/bundle';
+import AOS from 'aos';
+
+
+
+//swiper
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  allowTouchMove: false, //防止觸控滑動
+  centeredSlides: true,
+  speed: 10000,
+  loop: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+});
+
+
+//AOS
+AOS.init();
